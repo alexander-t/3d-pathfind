efbc7d1802c4444e0671e6a7b29ea04299709877
Alexander Tarlinder <alexander.tarlinder@crisp.se>
2020-09-07 07:13:38 +0200

    Can now detect objects while drawing the grid.

--- Assets/Scripts/AStarGrid.cs
+++ Assets/Scripts/AStarGrid.cs
@@ -7,20 +7,43 @@ public class AStarGrid : MonoBehaviour
     public float depth;
     public float gridSize;
 
+    public void Start()
+    {
+
+    }
     private void OnDrawGizmos()
     {
-        Gizmos.DrawWireCube(transform.position, new Vector3(width, 1, height));
+        foreach (GameObject go in GameObject.FindGameObjectsWithTag("Obstacle"))
+        {
+
+        }
+
         for (int z = 0; z < depth / gridSize; z++)
         {
             for (int y = 0; y < height / gridSize; y++)
             {
                 for (int x = 0; x < width / gridSize; x++)
                 {
-                    Gizmos.DrawWireCube(new Vector3(transform.position.x - (width / 2) + (x * gridSize) + (gridSize / 2),
+                    if (Physics.CheckSphere(new Vector3(transform.position.x - (width / 2) + (x * gridSize) + (gridSize / 2),
                         transform.position.z - (depth / 2) + (z * gridSize) + (gridSize / 2),
-                        transform.position.y - (height / 2) + (y * gridSize) + (gridSize / 2)), new Vector3(gridSize, gridSize, gridSize));
+                        transform.position.y - (height / 2) + (y * gridSize) + (gridSize / 2)), gridSize / 2))
+                    {
+                        Gizmos.color = Color.red;
+                        Gizmos.DrawCube(new Vector3(transform.position.x - (width / 2) + (x * gridSize) + (gridSize / 2),
+                         transform.position.z - (depth / 2) + (z * gridSize) + (gridSize / 2),
+                         transform.position.y - (height / 2) + (y * gridSize) + (gridSize / 2)), new Vector3(gridSize, gridSize, gridSize));
+                    }
+                    else
+                    {
+                        Gizmos.color = Color.white;
+                        Gizmos.DrawWireCube(new Vector3(transform.position.x - (width / 2) + (x * gridSize) + (gridSize / 2),
+                            transform.position.z - (depth / 2) + (z * gridSize) + (gridSize / 2),
+                            transform.position.y - (height / 2) + (y * gridSize) + (gridSize / 2)), new Vector3(gridSize, gridSize, gridSize));
+                    }
                 }
             }
         }
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(transform.position, new Vector3(width, depth, height));
     }
 }
